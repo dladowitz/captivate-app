3086d59a37d96360131445df41201bed06bb2a70
David Ladowitz <david@ladowitz.com>
2015-12-11 18:00:38 -0800

    adding showing tracking url

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,10 @@ class ContactsController < ApplicationController
     @contact = Contact.new contact_params
 
     if @contact.save
+      # Tried doing this as an :after_create in the model, but root_url wasn't available
+      @contact.showing_tracking_url__c = root_url + "contacts/#{@contact.id}/showing_trackings/new"
+      @contact.save
+
       redirect_to contacts_path
     else
       redirect_to new_contact_path
